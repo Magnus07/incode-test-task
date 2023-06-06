00422bb787d3e9313d48811b2ac5ec29e0e38776
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-06-06 18:38:42 +0300

    Added mongoose and mongoose-role

---
+++ models/UserModel.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+  subordinates: Array,
+});
+
+UserSchema.plugin(require("mongoose-role"), {
+  roles: ["public", "boss", "user", "admin"],
+  accessLevels: {
+    public: ["public", "user", "admin"],
+    anon: ["public"],
+    user: ["user", "admin", "boss"],
+    boss: ["admin", "boss"],
+    admin: ["admin"],
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
